501693a2296ea95bda6d4be14177617c54dc6237
YunSwain <destap@naver.com>
2023-10-08 14:37:18 +0900

    [Feat] 연락처 편집 뷰 TextField의 placeholder 설정, 기존 연락처 편집 시도할 시 TextField 자동완성 구현

--- ios-contact-manager/ios-contact-manager/Controller/EditViewController.swift
+++ ios-contact-manager/ios-contact-manager/Controller/EditViewController.swift
@@ -11,8 +11,25 @@ final class EditViewController: UIViewController {
     var contact: Contact?
     var indexPath: IndexPath?
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextFeild: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewComponents()
+    }
+    
+    private func configureViewComponents() {
+        nameTextField.placeholder = "이름을 입력해주세요."
+        ageTextFeild.placeholder = "나이를 입력해주세요."
+        phoneNumberTextField.placeholder = "전화번호를 입력해주세요."
+        
+        if let contact = contact {
+            nameTextField.text = contact.name
+            ageTextFeild.text = "\(contact.age)"
+            phoneNumberTextField.text = "\(contact.phoneNumber)"
+        }
     }
     
     @IBAction private func touchSaveBarButton(sender: UIBarButtonItem) {
